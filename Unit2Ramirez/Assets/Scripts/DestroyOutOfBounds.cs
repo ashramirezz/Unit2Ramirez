4ed3ecdd00da43d445f9fb7751224f9bd168e517
ashramirezz <ashleymr146@gmail.com>
2023-03-09 11:39:03 -0800

    Created new scripts
    created new scripts for my projectiles and made new prefabs.

---
+++ Unit2Ramirez/Assets/Scripts/DestroyOutOfBounds.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBounds : MonoBehaviour
+{
+    private float topBound = 30;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
--- Unit2Ramirez/Assets/Scripts/PlayerControl.cs
+++ Unit2Ramirez/Assets/Scripts/PlayerControl.cs
@@ -8,7 +8,8 @@ public class PlayerControl : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
-    public GameObject 
+
+    public GameObject projectilePrefab;
 
 
 
@@ -23,6 +24,11 @@ public class PlayerControl : MonoBehaviour
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
 
         horizontalInput = Input.GetAxis("Horizontal");
