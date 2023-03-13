5c0e36d66a8e2cd64de4afe37864f7813d824e6d
ashramirezz <ashleymr146@gmail.com>
2023-03-13 11:40:25 -0700

    Finished Basic Gameplay, started Bonus Track
    Finished the basic gameplay of my game and started the bonus track instructions of my game.

--- Unit2Ramirez/Assets/Scripts/DestroyOutOfBounds.cs
+++ Unit2Ramirez/Assets/Scripts/DestroyOutOfBounds.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class DestroyOutOfBounds : MonoBehaviour
 {
     private float topBound = 30;
+    private float lowerBound = 10;
     
 
     // Update is called once per frame
     void Update()
     {
+        //Destroys gamme objects when out of bounds of the plane/camera
         if (transform.position.z > topBound)
         {
             Destroy(gameObject);
         }
+        else if (transform.position.z < lowerBound)
+        {
+            Destroy(gameObject);
+        }
+
+        //Dsiplayes "Game Over" on the screen when game ends or player loses
+        Debug.Log("Game Over!");
+        Destroy(gameObject);
     }
 }
--- Unit2Ramirez/Assets/Scripts/PlayerControl.cs
+++ Unit2Ramirez/Assets/Scripts/PlayerControl.cs
@@ -7,11 +7,13 @@ public class PlayerControl : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
-    public float xRange = 10;
+    public float xRange = 10.0f;
 
     public GameObject projectilePrefab;
 
-
+    public float zMin;
+    public float zMax;
+    public float verticalInput;
 
     // Update is called once per frame
     void Update() 
@@ -21,17 +23,24 @@ public class PlayerControl : MonoBehaviour
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
+
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed);
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             //Launch a projectile from the player
             Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
 
-        horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        
     }
 }
